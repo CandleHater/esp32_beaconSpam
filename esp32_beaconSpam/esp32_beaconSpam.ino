2ae7a9128edb345dd39863c21d2c73e278a12d60
Candle <50766841+CandleHater@users.noreply.github.com>
2020-05-19 15:12:25 +0200

    Bitcoin SSIDs and small optimizations

--- esp32_beaconSpam/esp32_beaconSpam.ino
+++ esp32_beaconSpam/esp32_beaconSpam.ino
@@ -1,76 +1,70 @@
-// ===== Settings ===== //
-const uint8_t channels[] = {1, 6, 11}; // used Wi-Fi channels (available: 1-14)
-const bool wpa2 = true; // WPA2 networks
-const bool appendSpaces = true; // makes all SSIDs 32 characters long to improve performance
+// used Wi-Fi channels (available: 1-14)
+const uint8_t channels[] = {random(1, 14)}; // {2, 7, 13}
+const bool wpa2 = false;
 
 /*
-  SSIDs:
   - don't forget the \n at the end of each SSID!
   - max. 32 characters per SSID
   - don't add duplicates! You have to change one character at least
 */
 const char ssids[] PROGMEM = {
-  "Mom Use This One\n"
-  "Abraham Linksys\n"
-  "Benjamin FrankLAN\n"
-  "Martin Router King\n"
-  "John Wilkes Bluetooth\n"
-  "Pretty Fly for a Wi-Fi\n"
-  "Bill Wi the Science Fi\n"
-  "I Believe Wi Can Fi\n"
-  "Tell My Wi-Fi Love Her\n"
-  "No More Mister Wi-Fi\n"
-  "LAN Solo\n"
-  "The LAN Before Time\n"
-  "Silence of the LANs\n"
-  "House LANister\n"
-  "Winternet Is Coming\n"
-  "Ping’s Landing\n"
-  "The Ping in the North\n"
-  "This LAN Is My LAN\n"
-  "Get Off My LAN\n"
-  "The Promised LAN\n"
-  "The LAN Down Under\n"
-  "FBI Surveillance Van 4\n"
-  "Area 51 Test Site\n"
-  "Drive-By Wi-Fi\n"
-  "Planet Express\n"
-  "Wu Tang LAN\n"
-  "Darude LANstorm\n"
-  "Never Gonna Give You Up\n"
-  "Hide Yo Kids, Hide Yo Wi-Fi\n"
-  "Loading…\n"
-  "Searching…\n"
-  "VIRUS.EXE\n"
-  "Virus-Infected Wi-Fi\n"
-  "Starbucks Wi-Fi\n"
-  "Text ###-#### for Password\n"
-  "Yell ____ for Password\n"
-  "The Password Is 1234\n"
-  "Free Public Wi-Fi\n"
-  "No Free Wi-Fi Here\n"
-  "Get Your Own Damn Wi-Fi\n"
-  "It Hurts When IP\n"
-  "Dora the Internet Explorer\n"
-  "404 Wi-Fi Unavailable\n"
-  "Porque-Fi\n"
-  "Titanic Syncing\n"
-  "Test Wi-Fi Please Ignore\n"
-  "Drop It Like It’s Hotspot\n"
-  "Life in the Fast LAN\n"
-  "The Creep Next Door\n"
-  "Ye Olde Internet\n"
+  "🌮 🌮 🌮 🌮 🌮\n"
+  "Satoshi Nakamoto\n"
+  "To the Moon!\n"
+  "21.000.000 BTC\n"
+  "HODL\n"
+  "Money without masters\n"
+  "When Lambo, Sir?\n"
+  "⚡ ⚡ ⚡ ⚡ ⚡\n"
+  "Brrrrrrrrrrrrrrr..\n"
+  "Citadel Intranet\n"
+  "REKT\n"
+  "Read the Whitepaper\n"
+  "Craig Wright is a fraud\n"
+  "Buy Bitcoin\n"
+  "🌝 🌝 🌝 🌝 🌝\n"
+  "FUCK BANKS!\n"
+  "Alice and Bob's WLAN\n"
+  "6.15 BTC\n"
+  "Be a Chad\n"
+  "Bitcoin Bitcoin Bitcoin Bitcoin\n"
+  "🚀 🚀 🚀 🚀 🚀\n"
+  "HTTP 402 Payment Required\n"
+  "The Times 03/Jan/2009\n"
+  "The People's Money\n"
+  "Bitcoin fixes this\n"
+  "1 BTC = 1 BTC\n"
+  "Orange coin good, number go up\n"
+  "📈 📈 📈 📈 📈\n"
+  "Bitcoin is \"fuck you money\"\n"
+  "FOMO\n"
+  "bitcoin.org\n"
+  "Don't trust. Verify.\n"
+  "Run a Bitcoin node\n"
+  "Stacking Sats\n"
+  "🕯 🕯 🕯 🕯 🕯\n"
+  "Unconfiscatable\n"
+  "Buy the Dip\n"
+  "Lightning Network ⚡\n"
+  "Be your own Bank\n"
+  "DYOR\n"
+  "🐱‍💻\n"
+  "Hey, Hey, Hey! BitConneeeeee..\n"
+  "1337 h4x0r, j0 m4n!\n"
+  "BTC BTC BTC BTC BTC BTC\n"
+  "In it for the technology\n"
+  "Long BTC, 100x leverage\n"
 };
 
-
-#include "WiFi.h"
+#include <WiFi.h>
 
 extern "C" {
-#include "esp_wifi.h"
-  //typedef void (*freedom_outside_cb_t)(uint8 status);
-  //int wifi_register_send_pkt_freedom_cb(freedom_outside_cb_t cb);
-  //void wifi_unregister_send_pkt_freedom_cb(void);
-  //int wifi_send_pkt_freedom(uint8 *buf, int len, bool sys_seq);
+  #include "esp_wifi.h"
+
+  // typedef void (*freedom_outside_cb_t)(uint8 status);
+  // int wifi_register_send_pkt_freedom_cb(freedom_outside_cb_t cb);
+  // void wifi_unregister_send_pkt_freedom_cb(void);
+  // int wifi_send_pkt_freedom(uint8 *buf, int len, bool sys_seq);
   esp_err_t esp_wifi_set_channel(uint8_t primary, wifi_second_chan_t second);
   esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
 }
@@ -88,21 +82,21 @@ uint32_t packetRateTime = 0;
 
 // beacon frame definition
 uint8_t beaconPacket[109] = {
-  /*  0 - 3  */ 0x80, 0x00, 0x00, 0x00, // Type/Subtype: managment beacon frame
-  /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Destination: broadcast
-  /* 10 - 15 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
-  /* 16 - 21 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
+  /*  0 - 3  */ 0x80, 0x00, 0x00, 0x00,                         // Type/Subtype: managment beacon frame
+  /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,             // Destination: broadcast
+  /* 10 - 15 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,             // Source
+  /* 16 - 21 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,             // Source
 
   // Fixed parameters
-  /* 22 - 23 */ 0x00, 0x00, // Fragment & sequence number (will be done by the SDK)
+  /* 22 - 23 */ 0x00, 0x00,                                     // Fragment & sequence number (will be done by the SDK)
   /* 24 - 31 */ 0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00, // Timestamp
-  /* 32 - 33 */ 0xe8, 0x03, // Interval: 0x64, 0x00 => every 100ms - 0xe8, 0x03 => every 1s
-  /* 34 - 35 */ 0x31, 0x00, // capabilities Tnformation
+  /* 32 - 33 */ 0xe8, 0x03,                                     // Interval: 0x64, 0x00 => every 100ms - 0xe8, 0x03 => every 1s
+  /* 34 - 35 */ 0x31, 0x00,                                     // capabilities Tnformation
 
   // Tagged parameters
 
   // SSID parameters
-  /* 36 - 37 */ 0x00, 0x20, // Tag: Set SSID length, Tag length: 32
+  /* 36 - 37 */ 0x00, 0x20,                                     // Tag: Set SSID length, Tag length: 32
   /* 38 - 69 */ 0x20, 0x20, 0x20, 0x20,
   0x20, 0x20, 0x20, 0x20,
   0x20, 0x20, 0x20, 0x20,
@@ -110,29 +104,32 @@ uint8_t beaconPacket[109] = {
   0x20, 0x20, 0x20, 0x20,
   0x20, 0x20, 0x20, 0x20,
   0x20, 0x20, 0x20, 0x20,
-  0x20, 0x20, 0x20, 0x20, // SSID
+  0x20, 0x20, 0x20, 0x20,                                       // SSID
 
   // Supported Rates
-  /* 70 - 71 */ 0x01, 0x08, // Tag: Supported Rates, Tag length: 8
-  /* 72 */ 0x82, // 1(B)
-  /* 73 */ 0x84, // 2(B)
-  /* 74 */ 0x8b, // 5.5(B)
-  /* 75 */ 0x96, // 11(B)
-  /* 76 */ 0x24, // 18
-  /* 77 */ 0x30, // 24
-  /* 78 */ 0x48, // 36
-  /* 79 */ 0x6c, // 54
+  /* 70 - 71 */ 0x01, 0x08,                                     // Tag: Supported Rates, Tag length: 8
+  /* 72 */ 0x82,                                                // 1(B)
+  /* 73 */ 0x84,                                                // 2(B)
+  /* 74 */ 0x8b,                                                // 5.5(B)
+  /* 75 */ 0x96,                                                // 11(B)
+  /* 76 */ 0x24,                                                // 18
+  /* 77 */ 0x30,                                                // 24
+  /* 78 */ 0x48,                                                // 36
+  /* 79 */ 0x6c,                                                // 54
 
   // Current Channel
-  /* 80 - 81 */ 0x03, 0x01, // Channel set, length
-  /* 82 */      0x01,       // Current Channel
+  /* 80 - 81 */ 0x03, 0x01,                                     // Channel set, length
+  /* 82 */      0x01,                                           // Current Channel
 
   // RSN information
   /*  83 -  84 */ 0x30, 0x18,
   /*  85 -  86 */ 0x01, 0x00,
   /*  87 -  90 */ 0x00, 0x0f, 0xac, 0x02,
   /*  91 -  92 */ 0x02, 0x00,
-  /*  93 - 100 */ 0x00, 0x0f, 0xac, 0x04, 0x00, 0x0f, 0xac, 0x04, /*Fix: changed 0x02(TKIP) to 0x04(CCMP) is default. WPA2 with TKIP not supported by many devices*/
+
+  // Fix: changed 0x02(TKIP) to 0x04(CCMP) is default. WPA2 with TKIP not supported by many devices
+  /*  93 - 100 */ 0x00, 0x0f, 0xac, 0x04, 0x00, 0x0f, 0xac, 0x04,
+
   /* 101 - 102 */ 0x01, 0x00,
   /* 103 - 106 */ 0x00, 0x0f, 0xac, 0x02,
   /* 107 - 108 */ 0x00, 0x00
@@ -140,29 +137,38 @@ uint8_t beaconPacket[109] = {
 
 // goes to next channel
 void nextChannel() {
-  if (sizeof(channels) > 1) {
-    uint8_t ch = channels[channelIndex];
-    channelIndex++;
-    if (channelIndex > sizeof(channels)) channelIndex = 0;
+  if (sizeof(channels) < 2) {
+    return;
+  }
 
-    if (ch != wifi_channel && ch >= 1 && ch <= 14) {
-      wifi_channel = ch;
-      //wifi_set_channel(wifi_channel);
-      esp_wifi_set_channel(wifi_channel, WIFI_SECOND_CHAN_NONE);
-    }
+  uint8_t ch = channels[channelIndex];
+
+  channelIndex++;
+  if (channelIndex > sizeof(channels)) {
+    channelIndex = 0;
+  }
+
+  if (ch != wifi_channel && ch >= 1 && ch <= 14) {
+    wifi_channel = ch;
+
+    // wifi_set_channel(wifi_channel);
+    esp_wifi_set_channel(wifi_channel, WIFI_SECOND_CHAN_NONE);
   }
 }
 
 // generates random MAC
 void randomMac() {
-  for (int i = 0; i < 6; i++)
+  for (int i = 0; i < 6; i++) {
     macAddr[i] = random(256);
+  }
 }
 
 void setup() {
   // create empty SSID
-  for (int i = 0; i < 32; i++)
+  for (int i = 0; i < 32; i++) {
     emptySSID[i] = ' ';
+  }
+
   // for random generator
   randomSeed(1);
 
@@ -170,6 +176,7 @@ void setup() {
   packetSize = sizeof(beaconPacket);
   if (wpa2) {
     beaconPacket[34] = 0x31;
+
   } else {
     beaconPacket[34] = 0x21;
     packetSize -= 26;
@@ -177,29 +184,30 @@ void setup() {
 
   // generate random mac address
   randomMac();
-  
+
   // put your setup code here, to run once:
   Serial.begin(115200);
-  //say hi
-  Serial.println("Hello,NodeMCU!");
-  //change WiFi mode
+
+  // change WiFi mode
   WiFi.mode(WIFI_MODE_STA);
 
   // set channel
   esp_wifi_set_channel(channels[0], WIFI_SECOND_CHAN_NONE);
 
-  // print out saved SSIDs
-  Serial.println("SSIDs:");
+  // print out SSIDs
+  Serial.println("\n\nSSIDs:");
+
   int i = 0;
   int len = sizeof(ssids);
-  while(i < len){
-    Serial.print((char)pgm_read_byte(ssids + i));
+  while (i < len) {
+    Serial.print((char) pgm_read_byte(ssids + i));
     i++;
   }
+
+  Serial.println("\nspamming..\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   currentTime = millis();
 
   // send out SSIDs
@@ -217,8 +225,8 @@ void loop() {
     // go to next channel
     nextChannel();
 
+    // read out next SSID
     while (i < ssidsLen) {
-      // read out next SSID
       j = 0;
       do {
         tmp = pgm_read_byte(ssids + i + j);
@@ -245,42 +253,22 @@ void loop() {
       beaconPacket[82] = wifi_channel;
 
       // send packet
-      if (appendSpaces) {
-        for (int k = 0; k < 3; k++) {
-          //packetCounter += wifi_send_pkt_freedom(beaconPacket, packetSize, 0) == 0;
-          //Serial.printf("size: %d \n", packetSize);
-          packetCounter += esp_wifi_80211_tx(ESP_IF_WIFI_STA, beaconPacket, packetSize, 0) == 0;
-          delay(1);
-        }
-      }
-
-      // remove spaces
-      else {
-        uint16_t tmpPacketSize = (109 - 32) + ssidLen; // calc size
-        uint8_t* tmpPacket = new uint8_t[tmpPacketSize]; // create packet buffer
-        memcpy(&tmpPacket[0], &beaconPacket[0], 37 + ssidLen); // copy first half of packet into buffer
-        tmpPacket[37] = ssidLen; // update SSID length byte
-        memcpy(&tmpPacket[38 + ssidLen], &beaconPacket[70], 39); // copy second half of packet into buffer
-
-        // send packet
-        for (int k = 0; k < 3; k++) {
-          //packetCounter += wifi_send_pkt_freedom(tmpPacket, tmpPacketSize, 0) == 0;
-          packetCounter += esp_wifi_80211_tx(ESP_IF_WIFI_STA, tmpPacket, tmpPacketSize, 0) == 0;
-          delay(1);
-        }
-
-        delete tmpPacket; // free memory of allocated buffer
+      for (int k = 0; k < 3; k++) {
+        packetCounter += esp_wifi_80211_tx(ESP_IF_WIFI_STA, beaconPacket, packetSize, 0) == 0;
+        delay(2); // 1
       }
 
       i += j;
     }
   }
 
-  // show packet-rate each second
-  if (currentTime - packetRateTime > 1000) {
+  // show packet-rate
+  if (currentTime - packetRateTime > 3000) {
     packetRateTime = currentTime;
-    Serial.print("Packets/s: ");
-    Serial.println(packetCounter);
+
+    Serial.print(packetCounter);
+    Serial.println(" packets/s");
+
     packetCounter = 0;
   }
 }
